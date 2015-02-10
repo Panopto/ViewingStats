f0550086f8a3abd537146dc106591dc51f922fff
Panopto <jlin@panopto.com>
2015-02-09 21:27:41 -0800

    Initial commit
    Adding the initial project.
    
    Needs:
    - Cleanup
    - Better error handling
    - Pagination for API calls

---
+++ PanoptoCollectAllStats/MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Timers;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace PanoptoCollectAllStats
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static bool selfSigned = true; // Target server is a self-signed server
+        private static bool hasBeenInitialized = false;
+        private static System.Timers.Timer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (selfSigned)
+            {
+                // For self-signed servers
+                EnsureCertificateValidation();
+            }
+        }
+
+        /// <summary>
+        /// Creates a timer that calls method to get stats after short interval to
+        /// allow for UI changes
+        /// </summary>
+        /// <param name="sender">object that calls this method</param>
+        /// <param name="e">arguments</param>
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            LockAllFields();
+            Status.Content = "Quering for stats...";
+
+            ErrorLog.Text = "";
+
+            timer = new System.Timers.Timer(100);
+            timer.Elapsed += DispatchUpload;
+
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Disabling all fields
+        /// </summary>
+        private void LockAllFields()
+        {
+            UserID.IsEnabled = false;
+            UserPassword.IsEnabled = false;
+            Query.IsEnabled = false;
+        }
+
+        /// <summary>
+        /// Enabling all fields that are originally enabled
+        /// </summary>
+        private void FreeAllFields()
+        {
+            UserID.IsEnabled = true;
+            UserPassword.IsEnabled = true;
+            Query.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Calls the process stats method
+        /// </summary>
+        private void DispatchUpload(Object source, ElapsedEventArgs e)
+        {
+            timer.Stop();
+
+            Action uploadMethod = ProcessBackground;
+            Dispatcher.BeginInvoke(uploadMethod);
+        }
+
+        /// <summary>
+        /// Sets up BackgroundWorker for stats collection in background and starts BackgroundWorker
+        /// </summary>
+        private void ProcessBackground()
+        {
+            BackgroundWorker bgw = new BackgroundWorker();
+            bgw.DoWork += new DoWorkEventHandler(ProcessStatsRequest);
+            bgw.ProgressChanged += new ProgressChangedEventHandler(CreationStatus);
+            bgw.WorkerReportsProgress = true;
+            bgw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(CreationComplete);
+
+            object[] args = new object[] { UserID.Text, UserPassword.Password };
+
+            bgw.RunWorkerAsync(args);
+        }
+
+        /// <summary>
+        /// Make the request for stats
+        /// </summary>
+        /// <param name="sender">BackgroundWorker Object</param>
+        /// <param name="e">Arguments necessary to get the stats</param>
+        private void ProcessStatsRequest(object sender, DoWorkEventArgs e)
+        {
+
+            BackgroundWorker bgw = sender as BackgroundWorker;
+
+            // Variables needed to create user
+            object[] args = e.Argument as object[];
+            string userName = args[0] as string;
+            string password = args[1] as string;
+
+
+            string errorMessage = null;
+            string statsFound = ManagementWrapper.GetAllSessionStats(userName, password, out errorMessage);
+
+            // Write out the results
+            string fileName = "Stats_" + DateTime.Now.ToString("dd_MM_yyyy_hh_mm") + ".csv";
+            System.IO.File.WriteAllLines(@"C:\temp\" + fileName, new List<string> { statsFound });
+
+
+            // Handle overall status
+            bgw.ReportProgress(100, 1 + "~ Stats query complete.");
+
+        }
+
+        private void WriteToFile(string statsFound)
+        {
+            
+        }
+
+        /// <summary>
+        /// Updates status of user creation to UI
+        /// </summary>
+        /// <param name="sender">BackgroundWorker Object</param>
+        /// <param name="e">Arguments used to update status</param>
+        private void CreationStatus(object sender, ProgressChangedEventArgs e)
+        {
+            int errorCode = Convert.ToInt16((e.UserState as string).Split('~')[0]);
+            string msg = (e.UserState as string).Split('~')[1];
+
+            if (errorCode == -1)
+            {
+                Status.Content = msg;
+            }
+            else if (errorCode == 0)
+            {
+                Status.Content = "Creation process experienced error";
+                ErrorLog.Text = msg;
+            }
+            else
+            {
+                Status.Content = msg;
+            }
+        }
+
+        /// <summary>
+        /// Free fields in UI
+        /// </summary>
+        /// <param name="sender">BackgoundWorker Object</param>
+        /// <param name="e">Arguments to be used</param>
+        private void CreationComplete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            FreeAllFields();
+        }
+
+        //========================= Needed to use self-signed servers
+
+        /// <summary>
+        /// Ensures that our custom certificate validation has been applied
+        /// </summary>
+        public static void EnsureCertificateValidation()
+        {
+            if (!hasBeenInitialized)
+            {
+                ServicePointManager.ServerCertificateValidationCallback += new System.Net.Security.RemoteCertificateValidationCallback(CustomCertificateValidation);
+                hasBeenInitialized = true;
+            }
+        }
+
+        /// <summary>
+        /// Ensures that server certificate is authenticated
+        /// </summary>
+        private static bool CustomCertificateValidation(object sender, X509Certificate cert, X509Chain chain, System.Net.Security.SslPolicyErrors error)
+        {
+            return true;
+        }
+    }
+}
---
+++ PanoptoCollectAllStats/ManagementWrapper.cs
@@ -0,0 +1,173 @@
+﻿using PanoptoCollectAllStats.SessionManagement;
+using PanoptoCollectAllStats.UserManagement;
+using PanoptoCollectAllStats.UsageReporting;
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Sample C# that uses the Panopto PublicAPI
+/// This sample shows how to get all viewing stats for a site through the Panopto PublicAPI
+/// </summary>
+namespace PanoptoCollectAllStats
+{
+
+    public class ManagementWrapper
+    {
+        private static int MaxPerPage = 25;
+        private static Dictionary<string, string> AllUserIds = new Dictionary<string, string>();
+
+        public static string GetAllStatsForSession(string authUserKey, string authPassword, Guid sessionID, string sessionName, string folderName, double? sessionLength)
+        {
+            string sessionStats = String.Empty;
+
+            // Get detailed stats
+            // Permissions for user management
+            UsageReporting.AuthenticationInfo usageAuth = new UsageReporting.AuthenticationInfo()
+            {
+                UserKey = authUserKey,
+                Password = authPassword
+            };
+
+            // Sanitize the session name for a CSV
+            if (sessionName.Contains(","))
+            {
+                sessionName = sessionName.Replace(",", "");
+            }
+            if (folderName.Contains(","))
+            {
+                folderName = folderName.Replace(",", "");
+            }
+
+            UsageReportingClient urc = new UsageReportingClient();
+            UsageReporting.Pagination pagination = new UsageReporting.Pagination();
+            pagination.MaxNumberResults = MaxPerPage;
+            pagination.PageNumber = 0;
+            DateTime currentTime = DateTime.Now;
+
+            DetailedUsageResponse usageResponse = urc.GetSessionDetailedUsage(
+                usageAuth, 
+                sessionID, 
+                pagination, 
+                new DateTime(2015, 1, 1), // Years ago
+                currentTime.AddSeconds(-1)); // Today
+
+            if (   usageResponse != null
+                && usageResponse.TotalNumberResponses > 0)
+            {
+                foreach (DetailedUsageResponseItem responseItem in usageResponse.PagedResponses)
+                {
+                    string userId = responseItem.UserId.ToString();
+                    string username = userId;
+                    if (AllUserIds.ContainsKey(userId))
+                    {
+                        username = AllUserIds[userId];
+                    }
+                    else
+                    {
+                        // Get the username.
+                        username = GetUserNameFromId(authUserKey, authPassword, responseItem.UserId);
+                    }
+
+                    // Session ID, Session Name, username, start position, length, date, Folder
+                    string usageFormat = "{0}, {1}, {2} , {3}, {4}, {5}, {6}, {7} \n";
+                    string individualStatsString = String.Format(
+                        usageFormat,
+                        sessionID.ToString(),
+                        sessionName,
+                        username,
+                        responseItem.StartPosition,
+                        responseItem.SecondsViewed,
+                        responseItem.SecondsViewed / sessionLength * 100,
+                        responseItem.Time,
+                        folderName);
+                    sessionStats += individualStatsString;
+                }
+
+                if (usageResponse.TotalNumberResponses > usageResponse.PagedResponses.Length)
+                {
+                    // Get more data
+
+                }
+
+            }
+            else
+            {
+                string noneFormat = "{0}, {1}, none,,,,, {2}, \n";
+
+                sessionStats = String.Format(noneFormat, sessionID.ToString(), sessionName, folderName);
+            }
+
+            return sessionStats;
+        }
+
+        private static string GetUserNameFromId(string authUserKey, string authPassword, Guid userGuid)
+        {
+            string username = String.Empty;
+
+            UserManagement.AuthenticationInfo auth = new UserManagement.AuthenticationInfo();
+            auth.UserKey = authUserKey;
+            auth.Password = authPassword;
+
+            UserManagementClient umc = new UserManagementClient();
+            User[] users = umc.GetUsers(auth, new Guid[] { userGuid });
+
+            if (users != null
+                && users.Length >= 1
+                && !String.IsNullOrWhiteSpace(users[0].UserKey))
+            {
+                username = users[0].UserKey;
+            }
+
+            return username;
+        }
+
+        public static string GetAllSessionStats(string authUserKey, string authPassword, out string errorMessage)
+        {
+            string sessionStats = "Session ID, Session Name, Username, Start Position (sec), Length (sec), % Viewed, Date Viewed, Folder,\n";
+            //List<Guid> sessionIds = new List<Guid>();
+            errorMessage = "Query in progress.";
+
+            if (!String.IsNullOrWhiteSpace(authUserKey) && !String.IsNullOrWhiteSpace(authPassword))
+            {
+                try
+                {
+                    // Permissions for user management
+                    SessionManagement.AuthenticationInfo sessionAuth = new SessionManagement.AuthenticationInfo()
+                    {
+                        UserKey = authUserKey,
+                        Password = authPassword
+                    };
+
+                    SessionManagementClient smc = new SessionManagementClient();
+                    ListSessionsRequest request = new ListSessionsRequest();
+                    SessionManagement.Pagination pagination = new SessionManagement.Pagination();
+                    pagination.MaxNumberResults = MaxPerPage;
+                    pagination.PageNumber = 0;
+
+                    request.Pagination = pagination;
+                    ListSessionsResponse sessionsResponse = smc.GetSessionsList(sessionAuth, request, null);
+
+                    Session[] sessions = sessionsResponse.Results;
+                    foreach (Session session in sessions)
+                    {
+                        //sessionIds.Add(sessionGuid);
+                        sessionStats += GetAllStatsForSession(authUserKey, authPassword, session.Id, session.Name, session.FolderName, session.Duration);
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    errorMessage = e.Message;
+                }
+
+            }
+            else
+            {
+                errorMessage = "Please enter username and password.";
+            }
+
+            //return sessionIds;
+            return sessionStats;
+        }
+    }
+}
